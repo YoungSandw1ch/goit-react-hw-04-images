582b8735c0912967e5baf9fa54649dff7fcce378
Sergei Mikhailenko <sersus1991@gmail.com>
2023-03-03 23:13:47 +0200

    app with hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,30 +3,27 @@ import { GlobalStyle } from 'components/Common/GlobalStyle';
 import { theme } from 'constants/thema';
 import { Box } from './Common/Box';
 import { Searchbar } from './Searchbar';
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { getImages } from 'services';
 import { toast, ToastContainer } from 'react-toastify';
 import { toastOptions } from 'constants/toestOptions';
 import { ImageGallery } from './ImageGallery';
 import { Modal } from './Modal';
 
-export class App extends Component {
-  state = {
-    query: '',
-    page: 1,
-    hits: [],
-    totalHits: 0,
-    isLoading: false,
-    error: null,
-    modalImage: null,
-  };
+export const App = () => {
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [hits, setHits] = useState([]);
+  const [totalHits, setTotalHits] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [modalImage, setModalImage] = useState(null);
+  // const [error, setError] = useState(null);
 
-  async componentDidUpdate(_, pS) {
-    try {
-      const { query, page } = this.state;
-      if (!query) return;
-      if (pS.query !== query || pS.page !== page) {
-        this.setState({ isLoading: true });
+  useEffect(() => {
+    async function foo() {
+      try {
+        if (!query) return;
+        setIsLoading(true);
         const data = await getImages(query, page);
         const hits = data.hits;
 
@@ -34,56 +31,47 @@ export class App extends Component {
           toast.error('Ups, nothing to search', toastOptions);
         }
 
-        this.setState(state => ({
-          hits: [...state.hits, ...hits],
-          isLoading: false,
-          totalHits: data.totalHits,
-        }));
+        setHits(pHits => [...pHits, ...hits]);
+        setIsLoading(false);
+        setTotalHits(data.totalHits);
+      } catch (error) {
+        // setError(error);
+        console.log(error.message);
       }
-    } catch (error) {
-      this.setState({ error });
-      console.log(error.message);
     }
-  }
+    foo();
+  }, [page, query]);
 
-  setPage = () => {
-    this.setState(state => ({ page: state.page + 1 }));
+  const nextPage = () => {
+    setPage(page => page + 1);
   };
 
-  formSubmit = query => {
-    if (query === this.state.query) return;
-    this.setState({ query, page: 1, hits: [], error: null, totalHits: 0 });
+  const formSubmit = value => {
+    if (value === query) return;
+    setQuery(value);
+    setPage(1);
+    setHits([]);
+    // setError(null);
+    setTotalHits(0);
   };
 
-  openModal = modalImage => {
-    this.setState({ modalImage });
-  };
+  const hasMore = totalHits === hits.length && page > 1 ? false : true;
 
-  closeModal = () => {
-    this.setState({ modalImage: null });
-  };
-
-  render() {
-    const { formSubmit, setPage, closeModal, openModal } = this;
-    const { hits, isLoading, totalHits, page, modalImage } = this.state;
-    const hasMore = totalHits === hits.length && page > 1 ? false : true;
-
-    return (
-      <ThemeProvider theme={theme}>
-        <Box>
-          <Searchbar onSubmit={formSubmit} />
-          <ImageGallery
-            hits={hits}
-            setPage={setPage}
-            openModal={openModal}
-            isLoading={isLoading}
-            hasMore={hasMore}
-          />
-          <Modal closeModal={closeModal} image={modalImage} />
-        </Box>
-        <ToastContainer />
-        <GlobalStyle />
-      </ThemeProvider>
-    );
-  }
-}
+  return (
+    <ThemeProvider theme={theme}>
+      <Box>
+        <Searchbar onSubmit={formSubmit} />
+        <ImageGallery
+          hits={hits}
+          setPage={nextPage}
+          openModal={setModalImage}
+          isLoading={isLoading}
+          hasMore={hasMore}
+        />
+        <Modal closeModal={() => setModalImage(null)} image={modalImage} />
+      </Box>
+      <ToastContainer />
+      <GlobalStyle />
+    </ThemeProvider>
+  );
+};
